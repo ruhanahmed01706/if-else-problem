ae3a1333f7a5a512bdac43990c6f88dfcd6436af
ruhan <mdruhanahmed49@gmail.com>
2025-10-06 13:03:27 +0600

    problem4 slove

---
+++ problem4.js
@@ -0,0 +1,15 @@
+const largNum1 = 5;
+const largNum2 = 12;
+const largNum3 = 8;
+if(largNum1 > largNum2 && largNum1 > largNum3){
+    console.log("Large Number:",largNum1);
+    
+}
+else if(largNum2 > largNum1 && largNum2 > largNum3){
+    console.log("large Number:",largNum2);
+    
+}
+else{
+    console.log("Large Number:",largNum3);
+    
+}
