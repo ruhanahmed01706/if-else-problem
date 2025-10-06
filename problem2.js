aab7642fdb4471b0068e1f6e4f739fd56a11d12e
ruhan <mdruhanahmed49@gmail.com>
2025-10-06 13:00:07 +0600

    problem2 solve

---
+++ problem2.js
@@ -0,0 +1,15 @@
+// Problem 02
+
+const pnnumber = 5;
+if(pnnumber === 0){
+    console.log('Zero');
+    
+}
+else if(pnnumber > 0){
+    console.log('Positive');
+    
+}
+else if(pnnumber < 0){
+    console.log('Negative');
+    
+}
