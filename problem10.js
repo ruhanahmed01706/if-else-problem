42c6cd7d03ff97ec43efe3076f24c56af8794b16
ruhan <mdruhanahmed49@gmail.com>
2025-10-07 17:44:56 +0600

    problem 10 11 solve

---
+++ problem10.js
@@ -0,0 +1,12 @@
+const letter = 'b';
+// if(letter ==='a' || letter ==='e' || letter=== 'i' || letter === 'o' || letter === 'u'){
+//     console.log('Vowel');
+    
+// }
+// else{
+//     console.log('Consonant');
+    
+// }
+
+const result = (letter ==='a' || letter ==='e' || letter=== 'i' || letter === 'o' || letter === 'u') ? "vowel" : "consonat";
+console.log(result);
---
+++ problem11.js
@@ -0,0 +1,3 @@
+const number = 9;
+const result =(number % 7 ===0) ? "Multiple of 7" : (number%3===0) ? "Multiple 3" : "Not multiple 3 or 7";
+console.log(result);
