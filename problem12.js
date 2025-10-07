1ba94c18ed583149cca131137cc9113058a18236
ruhan <mdruhanahmed49@gmail.com>
2025-10-07 19:19:42 +0600

    problem 12 13 solve

---
+++ problem12.js
@@ -0,0 +1,20 @@
+const num1 = 10;
+const num2 = 5;
+
+const oprator = '+';
+
+if(oprator ==='+'){
+    console.log(num1 + num2);
+    }
+    else if(oprator === '-') {
+        console.log(num1 - num2);
+        
+    }
+    else if(oprator === '*'){
+        console.log(num1 * num2);
+        
+    }
+    else if(oprator === '/') {
+        console.log(num1/num2);
+        
+    }
---
+++ problem13.js
@@ -0,0 +1,3 @@
+const num = -8;
+const result =(num <0)? num * -1 : num;
+console.log(result);
